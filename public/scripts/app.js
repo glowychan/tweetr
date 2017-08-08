822cdec43e147abbfc025b9baa722b9a9a20f627
glowychan <glochan.uog@gmail.com>
2017-08-08 12:19:39 -0400

    Create starter code for dynamic tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+function renderTweets(tweets) {
+  let $renderedTweet = tweets.forEach(createTweetElement);
+  $('#tweets-container').append($renderedTweet);
+}
+
+/*
+function createTweetElement(tweet) {
+  let $tweet = $('<article>').addClass('tweet')
+  let tweetContent = $tweet.html(`${tweet.content.text}`);
+  // console.log($tweet);
+}
+*/
+
+function createTweetElement(tweet) {
+
+  // Create basic tweet DOM structure of tweet article
+  let $tweet = $('<article class="tweet">')
+               .append($('<header class="tweet-header">'))
+               .append($('<div class="tweet-content">'))
+               .append($('<footer class="tweet-footer">'));
+
+  // Create nested tags for tweet DOM structure
+  let header = $(".tweet-header").append($(`<img class="avatar" src="${tweet.user.avatars.large}" alt="avatar">`))
+                                 .append($(`<span class="name">${tweet.user.name}</span>`))
+                                 .append($(`<span class="name">${tweet.user.handle}</span>`));
+
+
+  // console.log($tweet);
+  console.log(header)
+  let tweetContent = $(".tweet-content").html(`<p>${tweet.content.text}</p>`);
+  //console.log(tweetContent);
+
+
+}
+
+
+
+
+
+$(document).ready(function() {
+  // Call function
+
+});
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,12 @@
-$(document ).ready(function() {
-    console.log( "ready!" );
-    $("textarea").keyup(function() {
-      // console.log(this.value.length);
-      let charLength = this.value.length;
-      let limit = 140;
+$(document).ready(function() {
+  $("textarea").keyup(function() {
+    let charLength = this.value.length;
+    let limit = 140;
 
-      if (limit - charLength >= 0) {
-        $(this).siblings('span').removeClass('exceed-limit').text(limit - charLength);
-      } else {
-        $(this).siblings('span').addClass('exceed-limit').text(limit - charLength);
-      }
-
-    });
+    if (limit - charLength >= 0) {
+      $(this).siblings('span').removeClass('exceed-limit').text(limit - charLength);
+    } else {
+      $(this).siblings('span').addClass('exceed-limit').text(limit - charLength);
+    }
+  });
 });
